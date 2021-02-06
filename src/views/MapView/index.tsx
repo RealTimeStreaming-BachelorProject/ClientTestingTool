c8640484d1aabb9654e5682cdbdd7a503d559d8a
Nicolai Gram <nicolaigram@gmail.com>
2021-02-06 16:55:23 +0100

    dno ..

--- src/views/MapView/index.tsx
+++ src/views/MapView/index.tsx
@@ -1,21 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDriverData as useDriverPosition } from "./useDriverData";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { iconPerson } from "./customMarker/marker";
-import "./customMarker/marker.css"
+import "./customMarker/marker.css";
 
 export default function MapView() {
   const position = useDriverPosition();
   return (
     <div>
-      {position}
       <MapContainer center={position} zoom={18} style={{ height: "100vh" }}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position} icon={iconPerson}>
-        </Marker>
+        <Marker position={position} icon={iconPerson}></Marker>
       </MapContainer>
     </div>
   );
