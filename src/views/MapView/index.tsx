c8552e7496ff2902328381a6423e52f3820ca825
Nicolai Gram <nicolaigram@gmail.com>
2021-02-07 13:25:45 +0100

    Add driver id

--- src/views/MapView/index.tsx
+++ src/views/MapView/index.tsx
@@ -5,16 +5,33 @@ import { iconPerson } from "./customMarker/marker";
 import "./customMarker/marker.css";
 
 export default function MapView() {
-  const position = useDriverPosition();
+  const [driverId, setDriverId] = useState("");
+  const [tempDriverId, setTempDriverId] = useState("");
+  const position = useDriverPosition(driverId);
   return (
     <div>
-      <MapContainer center={position} zoom={18} style={{ height: "100vh" }}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position} icon={iconPerson}></Marker>
-      </MapContainer>
+      {!driverId && (
+      <form onSubmit={e => {
+        e.preventDefault()
+        setDriverId(tempDriverId)
+      }}>
+        <label htmlFor="">driverId</label>
+        <input type="text" onChange={(e) => setTempDriverId(e.target.value)} value={tempDriverId} />
+        <button>Set</button>
+      </form>
+      )}
+      {!position && (
+        <div>No position yet</div>
+      )}
+      {position && (
+        <MapContainer center={position} zoom={18} style={{ height: "100vh" }}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position} icon={iconPerson}></Marker>
+        </MapContainer>
+      )}
     </div>
   );
 }
--- src/views/MapView/useDriverData.tsx
+++ src/views/MapView/useDriverData.tsx
@@ -2,20 +2,20 @@ import React, { useEffect, useState } from 'react'
 import SocketIOClient from 'socket.io-client'
 const ENDPOINT = "http://localhost:5001"
 
-export const useDriverData = (): [number, number] => {
+export const useDriverData = (driverId: string): [number, number] | undefined => {
     const [socket, setSocket] = useState<SocketIOClient.Socket | null>(null)
-    const [position, setPosition] = useState("");
+    const [position, setPosition] = useState<[number, number] | undefined>(undefined)
     useEffect(() => {
         if (socket) return;
         setSocket(SocketIOClient(ENDPOINT))
     })
     useEffect(() => {
         if (!socket) return;
-        socket.on("last-position", (lastPosition: string) => {
+        if (!driverId) return;
+        socket.on("last-position", (lastPosition: [number, number]) => {
             setPosition(lastPosition)
         })
     },[socket])
     
-    return [55.37651, 10.43341]
-    // return position
+    return position
 }
