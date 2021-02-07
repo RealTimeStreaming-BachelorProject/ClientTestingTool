30f4b7b91d415142e999617e46206257f829abb3
Nicolai Gram <nicolaigram@gmail.com>
2021-02-07 16:46:04 +0100

    Implement new server structure

--- src/views/MapView/useDriverData.tsx
+++ src/views/MapView/useDriverData.tsx
@@ -1,25 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import SocketIOClient from 'socket.io-client'
-const ENDPOINT = "http://localhost:5001"
+import React, { useEffect, useState } from "react";
+import SocketIOClient from "socket.io-client";
+const ENDPOINT = "http://localhost:5001/clients";
 
-export const useDriverData = (driverId: string): [number, number] | undefined => {
-    const [socket, setSocket] = useState<SocketIOClient.Socket | null>(null)
-    const [position, setPosition] = useState<[number, number] | undefined>(undefined)
-    useEffect(() => {
-        if (socket) return;
-        setSocket(SocketIOClient(ENDPOINT))
-    })
-    useEffect(() => {
-        if (!socket) return;
-        socket.emit("subscribe-to-driver", driverId);
-    },[driverId])
-    useEffect(() => {
-        if (!socket) return;
-        if (!driverId) return;
-        socket.on("last-position", (lastPosition: [number, number]) => {
-            setPosition(lastPosition)
-        })
-    },[socket])
-    
-    return position
-}
+export const useDriverData = (
+  driverId: string
+): [number, number] | undefined => {
+  const [socket, setSocket] = useState<SocketIOClient.Socket | null>(null);
+  const [position, setPosition] = useState<[number, number] | undefined>(
+    undefined
+  );
+  useEffect(() => {
+    if (socket) return;
+    setSocket(SocketIOClient(ENDPOINT));
+  });
+
+  useEffect(() => {
+    if (!socket) return;
+    socket.emit("subscribe-to-driver-coordinates", driverId);
+  }, [driverId]);
+
+  useEffect(() => {
+    if (!socket) return;
+    socket.on("latest-coordinates", (lastPosition: [number, number]) => {
+      console.log(lastPosition);
+      setPosition(lastPosition);
+    });
+  }, [socket]);
+
+  return position;
+};
