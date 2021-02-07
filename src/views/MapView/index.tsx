db415352a727e03e7ecab37da4c94e7e6a3d2cee
Nicolai Gram <nicolaigram@gmail.com>
2021-02-07 16:55:42 +0100

    Move map when coordinates update

--- src/views/MapView/index.tsx
+++ src/views/MapView/index.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDriverData as useDriverPosition } from "./useDriverData";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import { iconPerson } from "./customMarker/marker";
 import "./customMarker/marker.css";
 
@@ -11,20 +11,25 @@ export default function MapView() {
   return (
     <div>
       {!driverId && (
-      <form onSubmit={e => {
-        e.preventDefault()
-        setDriverId(tempDriverId)
-      }}>
-        <label htmlFor="">driverId</label>
-        <input type="text" onChange={(e) => setTempDriverId(e.target.value)} value={tempDriverId} />
-        <button>Set</button>
-      </form>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setDriverId(tempDriverId);
+          }}
+        >
+          <label htmlFor="">driverId</label>
+          <input
+            type="text"
+            onChange={(e) => setTempDriverId(e.target.value)}
+            value={tempDriverId}
+          />
+          <button>Set</button>
+        </form>
       )}
-      {!position && (
-        <div>No position yet</div>
-      )}
+      {!position && <div>No position yet</div>}
       {position && (
         <MapContainer center={position} zoom={18} style={{ height: "100vh" }}>
+          <ChangeView center={position} zoom={18} />
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -34,4 +39,18 @@ export default function MapView() {
       )}
     </div>
   );
+}
+
+function ChangeView({ center, zoom }: any) {
+  /*
+    This component allows to change the center of the map programatically.
+    https://stackoverflow.com/questions/64665827/react-leaflet-center-attribute-does-not-change-when-the-center-state-changes
+  */
+  const map = useMap();
+  useEffect(() => {
+    if (!center) return;
+    const animationSeconds = 1;
+    map.flyTo(center, zoom, { duration: animationSeconds });
+  }, [center]);
+  return null;
 }
