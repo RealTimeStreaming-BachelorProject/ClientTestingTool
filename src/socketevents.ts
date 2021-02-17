c6f4645b74815c64810bd9f58bf0f6a7b787b3c6
omvk97 <olvan18@student.sdu.dk>
2021-02-17 09:11:09 +0100

    refactor

--- src/socketevents.ts
+++
@@ -1,5 +0,0 @@
-export const NEW_COORDINATES = "new_coordinates";
-export const LATEST_COORDINATES = "latest_coordinates";
-export const DRIVER_INIT = "driver_init";
-export const DELIVERY_START = "delivery_start";
-export const SUBSCRIBE_TO_DRIVER = "subscribe_to_driver_coordinates"
