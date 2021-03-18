df9fe772c5efe50df3cd3a46dbff5cc3a0d8f71d
Nicolai Gram <nicolaigram@gmail.com>
2021-03-18 14:52:39 +0100

    Fix spacing

--- userTool/src/views/PackageView/index.tsx
+++ userTool/src/views/PackageView/index.tsx
@@ -14,27 +14,27 @@ export default function PackageView() {
     <Container>
       <Section>
         <h2>Package</h2>
-        <div>
+        <TextGroup>
           <Text
             label="Expected delivery"
             value={format(new Date(data.expectedDelivery), "dd/MM-yyyy HH:mm")}
           />
           <Text label="Weight" value={data.weightKg + " kg"} />
-        </div>
+        </TextGroup>
       </Section>
       <Section>
         <h2>Receiver</h2>
-        <div>
+        <TextGroup>
           <Text label="Address" value={data.receiverAddress} />
           <Text label="Name" value={data.receiverName} />
-        </div>
+        </TextGroup>
       </Section>
       <Section>
         <h2>Sender</h2>
-        <div>
+        <TextGroup>
           <Text label="Address" value={data.senderAddress} />
           <Text label="Name" value={data.senderName} />
-        </div>
+        </TextGroup>
       </Section>
       <Section>
         <h2>Events</h2>
@@ -51,15 +51,21 @@ const Container = styled.div`
   justify-content: center;
   width: 420px;
   padding: 40px;
-  border: 1px solid rgba(0,0,0,0.23);
+  border: 1px solid rgba(0, 0, 0, 0.23);
   border-radius: 40px;
 `;
 
 const Section = styled.div`
   display: flex;
   flex-direction: column;
+`;
+
+const TextGroup = styled.div`
+  display: flex;
   div {
-    flex-grow: 0;
-    margin-right: 4px;
+    flex-grow: 1;
+    &:not(:last-of-type) {
+      margin-right: 4px;
+    }
   }
 `;
